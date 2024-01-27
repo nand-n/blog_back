1325d4ce5ecb6b5f3245dd3d28bd12e8ef921764
Nahom Debele <nahomdebele002@gmail.com>
2024-01-27 10:50:54 +0300

    taking a break it is non sence error

--- src/config/mongoDb.js
+++ src/config/mongoDb.js
@@ -4,7 +4,7 @@ const logger = require('../config/logger');
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
     logger.info('Connected to MongoDB through Mongoose');
   } catch (error) {
     logger.error(`MongoDB connection error: ${error.message}`);
