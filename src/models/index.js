07085078e69d1a00b0532f1289214311c9e5f343
Nahom Debele <nahomdebele002@gmail.com>
2024-01-29 22:28:06 +0300

    added the goal model

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,5 @@
 module.exports.Song = require('./song.model');
 module.exports.Blog=require('./blog.model')
 module.exports.Note=require('./note.model')
+module.exports.Goal=require('./goal.mode')
+
