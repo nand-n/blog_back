a8b56e9d1687f2becf06ca606e1ea8f1686bc135
Nahom Debele <nahomdebele002@gmail.com>
2024-02-04 23:44:41 +0300

    added image upload for the create and update blogs

--- src/app.js
+++ src/app.js
@@ -22,7 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // enable cors
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.options('*', cors());
 
 // v1 api routes
