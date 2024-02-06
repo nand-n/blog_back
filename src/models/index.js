f3ef65be5c0d3d7732ef4bd4f734cfc0dc167f6a
Nahom Debele <nahomdebele002@gmail.com>
2024-02-06 21:42:10 +0300

    added a catagory model and working on note handler service and controrller

--- src/models/index.js
+++ src/models/index.js
@@ -2,4 +2,6 @@ module.exports.Song = require('./song.model');
 module.exports.Blog=require('./blog.model')
 module.exports.Note=require('./note.model')
 module.exports.Performance=require('./goal.mode')
+module.exports.Catagory=require('./catagory.model')
+
 
--- src/services/index.js
+++ src/services/index.js
@@ -3,4 +3,4 @@ module.exports.SongService = require('./song.service');
 module.exports.BlogService = require('./blog.service')
 module.exports.NoteService = require('./note.service')
 module.exports.PerformanceService = require('./performance.service')
-
+module.exports.CatagoryService = require('./catagory.service')
