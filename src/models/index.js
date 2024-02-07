91b49d3e1d8eb69edd365c19f722f8f5c498317a
Nahom Debele <nahomdebele002@gmail.com>
2024-02-07 22:19:41 +0300

    added a project controller

--- src/models/index.js
+++ src/models/index.js
@@ -3,5 +3,7 @@ module.exports.Blog=require('./blog.model')
 module.exports.Note=require('./note.model')
 module.exports.Performance=require('./goal.mode')
 module.exports.Catagory=require('./catagory.model')
+module.exports.Project=require('./project.model')
+
 
 
