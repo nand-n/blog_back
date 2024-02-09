c1e860b9a758c39c7576bb8fad664ff2bd3a6f98
Nahom Debele <nahomdebele002@gmail.com>
2024-02-09 22:43:48 +0300

    fixing the catagory

--- src/models/catagory.model.js
+++ src/models/catagory.model.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
   name: String,
+  type: {
+    type: String,
+    enum: ['finance', 'blog', 'notes' , 'project', 'accounting'], 
+  },
+  
 });
 
 const Category = mongoose.model('Category', categorySchema);
