dea4a8c458f615c653610113a1bed14c9af81307
nahom <nahomdebele002@gmail.com>
2024-04-17 22:16:31 +0300

    get catagory added

--- src/controllers/catagory.controller.js
+++ src/controllers/catagory.controller.js
@@ -4,7 +4,15 @@ const catchAsync = require("../utils/catchAsync");
 const getAllCategories =  catchAsync(async (req, res) => {
     const categories = await CatagoryService.getAllCatagories();
     res.json(categories);
+}
+
+)
+
+const getCategory =  catchAsync(async (req, res) => {
+    const categories = await CatagoryService.getCatagory(req.params.id);
+    res.json(categories);
 })
+
 
 const createCategory =  catchAsync(async (req, res) => {
     const category = await CatagoryService.createCatagory(req.body);
@@ -23,5 +31,6 @@ module.exports ={
     getAllCategories,
     createCategory,
     deleteCategory,
-    updateCategory
+    updateCategory,
+    getCategory
 }
