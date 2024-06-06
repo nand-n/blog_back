aeb9b125ba7a3d3846426c341f9cc64c3925b78a
Nahi002 <nahomdebele002@gmail.com>
2024-06-06 16:31:58 +0300

    pfoject related thing added

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,29 +1,14 @@
 const redisClient = require("../config/redis");
 const { ProjectService } = require("../services");
 const catchAsync = require("../utils/catchAsync");
-const upload = require("../utils/fileUpload");
-const cloudinary = require('cloudinary').v2;
-
 
 const getAllProjects =  catchAsync(async (req, res) => {
       const projects = await ProjectService.getAllProjects();
-      const client = redisClient.getClient();
-      client.setEx(
-        req.originalUrl,
-        3000,
-        JSON.stringify({ message: "Data from cache", projects })
-      );
       res.json(projects);
 })
   const getProjectById = catchAsync (async(req, res) => {
     try {
       const project = await ProjectService.getProjectById(req.params.id);
-      const client = redisClient.getClient();
-      client.setEx(
-        req.originalUrl,
-        3000,
-        JSON.stringify({ message: "Data from cache", project })
-      );
       res.json(project);
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -32,25 +17,13 @@ const getAllProjects =  catchAsync(async (req, res) => {
   
   const createProject = catchAsync(async (req, res) => {
     try {
-        // const imageUrl = req.file ? `http://localhost:3000/uploads/${req.file.filename}` : '';
-        const fileStr = req.body.data;
-        const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
-          upload_preset: 'ml_default',
-        });
 
-    const newprojectData = {
-      ...req.body,
-      // imageUrl: imageUrl,
-      imageUrl: uploadedResponse.secure_url,
-
-    };
-      const newproject = await ProjectService.createProject(newprojectData);
-      // redisClient.del('/projects');
-      res.status(201).json(newproject);
+      const newproject = await ProjectService.createProject({...req.body } );
+      res.status(200).json(newproject);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-  })
+  });
   
   const updateProject = catchAsync(async (req, res) => {
     try {
@@ -60,8 +33,9 @@ const getAllProjects =  catchAsync(async (req, res) => {
       ...req.body,
       imageUrl: imageUrl,
     };
+    
       const updatedproject = await ProjectService.updateProject(req.params.id,updatedprojectData);
-      // redisClient.del('/projects');
+      
       res.json(updatedproject);
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -71,7 +45,7 @@ const getAllProjects =  catchAsync(async (req, res) => {
   const deleteProject = catchAsync(async (req, res) => {
     try {
       const deletedproject = await ProjectService.deleteProject(req.params.id);
-      // redisClient.del('/projects');
+      
       res.json(deletedproject);
     } catch (error) {
       res.status(500).json({ error: error.message });
