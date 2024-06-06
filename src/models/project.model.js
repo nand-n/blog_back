666f80734afbbe8204cc29a58eea29d2fb3d9185
Nahi002 <nahomdebele002@gmail.com>
2024-06-06 16:32:10 +0300

    added image values

--- src/models/project.model.js
+++ src/models/project.model.js
@@ -4,6 +4,10 @@ const projectSchema = new mongoose.Schema({
   title: String,
   description: String,
   imageUrl: String,
+  descImageUrl1: String,
+  descImageUrl2: String,
+  description1: String,
+description2: String,
   topics: [
     {
       name: String,
