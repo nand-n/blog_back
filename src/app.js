6f87dd849f8d63cc6edbe120aa54546b32880be4
Nahi002 <nahomdebele002@gmail.com>
2024-06-10 17:13:51 +0300

    added a asset limit

--- src/app.js
+++ src/app.js
@@ -14,6 +14,9 @@ const app = express();
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 // set security HTTP headers
 app.use(helmet());
 
@@ -28,15 +31,14 @@ app.use(cors());
 app.use(cors({
   origin: '*'
 }));
-// app.use(cors({
-//   origin: '*'
-// }));
+app.use(cors({
+  origin: '*'
+}));
 // app.options('*', cors());
 
 // Serve static files from the 'public' directory
+
 // v1 api routes
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use('/api/v1', routes);
 app.use('/uploads',express.static( path.join(__dirname, 'uploads')));
 
